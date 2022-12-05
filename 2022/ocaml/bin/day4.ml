9125f15921350d9673ebc5cc29fac1aeaa6680ac
Dennis Dang <dangggdennis@gmail.com>
2022-12-04 23:06:58 -0500

    prep day4

---
+++ 2022/ocaml/bin/day4.ml
@@ -0,0 +1,13 @@
+let part1 _lines = 0
+let part2 _lines = 0
+
+let () =
+  let lines = Lib.extract_lines "bin/day3.txt" in
+  print_endline "day 4";
+  print_string "part 1: ";
+  part1 lines |> print_int;
+  print_newline ();
+  print_string "part 2: ";
+  part2 lines |> print_int;
+  print_newline ();
+  ()
