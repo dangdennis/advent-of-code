13761d160c8f32b0612eaa86e9550b7b7b412807
Dennis Dang <dangggdennis@gmail.com>
2022-12-06 00:22:42 -0500

    day4 part2

--- 2022/ocaml/bin/day4.ml
+++ 2022/ocaml/bin/day4.ml
@@ -1,22 +1,20 @@
-let part1 lines =
+let to_pairs l =
   let to_pair s =
     match String.split_on_char '-' s with
     | [] -> failwith "Not pairs"
     | _ :: [] -> failwith "Not pairs"
     | fst :: snd :: _ -> (int_of_string fst, int_of_string snd)
   in
-
-  let to_pairs l =
-    let sections = l |> String.split_on_char ',' in
-    let left, right =
-      match sections with
-      | [] -> failwith "Missing both sections"
-      | _ :: [] -> failwith "Missing a section"
-      | fst :: snd :: _ -> (fst |> to_pair, snd |> to_pair)
-    in
-    (left, right)
+  let sections = l |> String.split_on_char ',' in
+  let left, right =
+    match sections with
+    | [] -> failwith "Missing both sections"
+    | _ :: [] -> failwith "Missing a section"
+    | fst :: snd :: _ -> (fst |> to_pair, snd |> to_pair)
   in
+  (left, right)
 
+let part1 lines =
   let section_pairs = lines |> List.map to_pairs in
 
   let num_contained_pairs =
@@ -26,10 +24,8 @@ let part1 lines =
            let is_contained =
              match section_pair with
              | (lower1, upper1), (lower2, upper2)
-               when lower1 <= lower2 && upper1 >= upper2 ->
-                 1
-             | (lower1, upper1), (lower2, upper2)
-               when lower2 <= lower1 && upper2 >= upper1 ->
+               when (lower1 <= lower2 && upper1 >= upper2)
+                    || (lower2 <= lower1 && upper2 >= upper1) ->
                  1
              | _ -> 0
            in
@@ -39,7 +35,24 @@ let part1 lines =
 
   num_contained_pairs
 
-let part2 _lines = 0
+let part2 lines =
+  let section_pairs = lines |> List.map to_pairs in
+  let num_overlapping_pairs =
+    section_pairs
+    |> List.fold_left
+         (fun num section_pair ->
+           let is_overlapping =
+             match section_pair with
+             | (lower1, upper1), (lower2, upper2)
+               when min upper2 upper1 - max lower2 lower1 + 1 > 0 ->
+                 1
+             | _ -> 0
+           in
+           num + is_overlapping)
+         0
+  in
+
+  num_overlapping_pairs
 
 let () =
   let lines = Lib.extract_lines "bin/day4.txt" in
