44aea067ef1955796194af14f7f9c99ee06ea5f2
Dennis Dang <dangggdennis@gmail.com>
2022-12-10 02:12:35 -0500

    fail day7 ocaml

--- 2022/ocaml/bin/day7.ml
+++ 2022/ocaml/bin/day7.ml
@@ -1,13 +1,62 @@
-let part1 _lines = 0
-let part2 _lines = 0
+type tree_node = {
+  name : string;
+  size : int;
+  parent : tree_node option;
+  kids : tree_node list;
+}
+
+let rec total_size node =
+  List.fold_left (fun size kid -> size + total_size kid) node.size node.kids
+
+let rec to_root node =
+  match node.parent with Some p -> to_root p | None -> node
+
+let update_parent_kids kid =
+  match kid.parent with
+  | Some p ->
+      {
+        p with
+        kids = List.map (fun k -> if k.name = kid.name then kid else k) p.kids;
+      }
+  | None -> failwith "err"
+
+let to_tree lines =
+  List.map (String.split_on_char ' ') (List.tl lines)
+  |> List.fold_left
+       (fun node toks ->
+         match toks with
+         | [ "$"; "cd"; ".." ] -> update_parent_kids node
+         | [ "$"; "cd"; dir ] ->
+             let kid = List.find (fun kid -> kid.name = dir) node.kids in
+             { kid with parent = Some node }
+         | [ "dir"; dir ] ->
+             let kid = { name = dir; size = 0; parent = None; kids = [] } in
+             { node with kids = List.cons kid node.kids }
+         | [ "$"; "ls" ] -> node
+         | [ size; _ ] -> { node with size = node.size + int_of_string size }
+         | _ -> failwith "err")
+       { name = "/"; size = 0; parent = None; kids = [] }
+  |> update_parent_kids |> to_root
+
+let rec find f root =
+  List.fold_left
+    (fun l node -> l @ find f node)
+    (if f root then [ root ] else [])
+    root.kids
+
+let part1 root =
+  find (fun kid -> total_size kid <= 100000) root
+  |> List.fold_left (fun sum kid -> sum + total_size kid) 0
+
+let part2 root =
+  let free = 70000000 - total_size root in
+  let need = 30000000 - free in
+  find (fun kid -> total_size kid >= need) root
+  |> List.map (fun kid -> total_size kid)
+  |> List.sort compare |> List.hd
 
 let () =
-  let lines = Lib.extract_lines "bin/day7.txt" in
-  print_endline "day 6";
-  print_string "part 1: ";
-  part1 lines |> print_int;
-  print_newline ();
-  print_string "part 2: ";
-  part2 lines |> print_int;
-  print_newline ();
-  ()
+  let tree = Arg.read_arg "bin/day7.txt" |> Array.to_list |> to_tree in
+
+  part1 tree |> string_of_int |> print_endline
+(* part2 tree |> string_of_int |> print_endline *)
